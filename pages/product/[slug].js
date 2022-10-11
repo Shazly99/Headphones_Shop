ff671af763544632ae9e2bbad012d8e3b0665033
Shazly99 <ahmed.y.elshazly@gmail.com>
2022-10-11 13:58:05 +0200

    commit

--- pages/product/[slug].js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const slug = () => {
-  return (
-    <div>slug</div>
-  )
-}
---
+++ pages/product/[slug].jsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react'
+import { client, urlFor } from './../../lib/client';
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import Product from './../../components/Product';
+const productDetails = ({ products, product }) => {
+  let { image, name, details, price } = product;
+  const [Index, setIndex] = useState(0)
+  return (
+ 
+      <div>
+        <div className="product-detail-container">
+          <div>
+            <div className="image-container">
+              <img 
+              src={urlFor(image && image[Index])} 
+              className="product-detail-image" />
+            </div>
+            <div className="small-images-container">
+              {
+                image?.map((item, i) => (
+                  <img
+                    className={
+                      i===Index? 'small-image selected-image':
+                      "small-image"
+                    }
+                    key={i}
+                    src={urlFor(item)}
+                    onMouseEnter={()=>setIndex(i)}
+                  />
+                ))
+              }
+            </div>
+          </div>
+          <div className="product-detail-desc">
+            <h1>{name}</h1>
+            <div className="reviews">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+            <h4>Details: </h4>
+            <p>{details} </p>
+            <p className="price">${price}</p>
+            <div className="quantity">
+              <h3>Quantity: </h3>
+              <p className="quantity-desc">
+                <span className='minus' onClick="">
+                  <AiOutlineMinus />
+                </span>
+                <span className='num  '  >
+                  0
+                </span>
+                <span className='plus' onClick="">
+                  <AiOutlinePlus />
+                </span>
+              </p>
+            </div>
+            <div className="buttons">
+              <button type='button' className='add-to-cart'>Add To Cart</button>
+              <button type='button' className='buy-now'>Buy Now</button>
+
+            </div>
+          </div>
+        </div>
+        <div className="maylike-products-wrapper">
+          <h2>You may also like</h2>
+          <div className="marquee">
+            <div className="maylike-products-container track">
+              {
+                products.map((item)=>(
+                  <Product key={item._id} product={item} />
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+
+  
+  )
+}
+
+export default productDetails
+
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product" ]{
+    slug{
+      current
+    }
+  }`
+  const products = await client.fetch(query)
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current
+    }
+  }))
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type ==  "product" && slug.current == '${slug}'][0]`
+  const product = await client.fetch(query)
+
+  const productsQuery = `*[_type == "product"]`
+  const products = await client.fetch(productsQuery)
+
+  return {
+    props: { products, product }
+  }
+}
+// ‘Set your mind ablaze with boAt Rockerz 450 — our slick headphones that offer immersive sound
+// quality and add luxury to your sound. Propelled by crystal clear 4Omm dynamic divers, slip into
+// an alterate HO immersive audio realty, The soft cornered matte black fish allows for a
+// comfortable fi, propagated by plush foam in adaptive and adjustable design. Choose your mode,
+// {90 wireless with Bluetooth V42 or connect an aux wire that doesnt cause any drain on the
+// ‘300mAh rechargeable lithium battery.
